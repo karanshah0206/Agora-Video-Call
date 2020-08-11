c4ec0fe46b5168b7d78bf14ac2eaac0a7e3c8414
Karan Shah <karanshah0206@outlook.com>
2020-08-11 20:03:32 +0530

    Updated ID and Token

--- src/utils/Settings.js
+++ src/utils/Settings.js
@@ -7,7 +7,8 @@ export const RESOLUTION_ARR = {
 
 export const SHARE_ID = 1;
 
-export const APP_ID = "<#YOUR APP ID HERE#>";
+export const APP_ID = "c0041179099d492fa2dafcc82ec735c0";
 
 // Assign Token to null if you have not enabled app certificate
-export const Token = "<#YOUR TEMP TOKEN HERE#>";
+// export const Token = "<#YOUR TEMP TOKEN HERE#>";
+export const Token = null;
